435082f1dc035496f4fd6264b01d155464b2847a
robsonleitao <robsonleitao26@gmail.com>
2024-03-14 16:04:44 -0300

    criando a estilização do modal de novas transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,42 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SeachForm } from "./components/SearchForm";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <>
-        <Header />
+            <Header />
+            <Summary />
+
+            <TransactionsContainer>
+                <SeachForm />
+
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width='50%'>Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12000
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/03/2023</td>
+                        </tr>
+                        <tr>
+                            <td width='50%'>Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    -R$ 12000
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/03/2023</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </>
     )
 }
